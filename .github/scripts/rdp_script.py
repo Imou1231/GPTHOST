57a8a4cea157c164a5cc11b27f2cc78b13a97d08
Imou1231 <101591865+Imou1231@users.noreply.github.com>
2023-08-18 23:22:38 +0545

    Update rdp_script.py

--- .github/scripts/rdp_script.py
+++ .github/scripts/rdp_script.py
@@ -1,1 +1,33 @@
+import pyautogui
+import time
+import os
+
+def main():
+    # Open the Start menu
+    pyautogui.press('win')
+    time.sleep(2)
+
+    # Search for Remote Desktop
+    pyautogui.write('remote desktop')
+    pyautogui.press('enter')
+    time.sleep(2)
+
+    # Click on Remote Desktop Connection
+    pyautogui.click(50, 190)
+    time.sleep(2)
+
+    # Click on Computer field
+    pyautogui.click(760, 460)
+    time.sleep(2)
+
+    # Type the IP address of the RDP server
+    pyautogui.write('YOUR_RDP_SERVER_IP')
+    pyautogui.press('enter')
+    time.sleep(2)
+
+    # Perform other necessary interactions (username, password, etc.)
+    # This part depends on your RDP setup
+
+if __name__ == "__main__":
+    main()
 
