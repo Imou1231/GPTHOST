d5ec75c49d44947a3b87d094f187ba9baadb6107
Imou1231 <101591865+Imou1231@users.noreply.github.com>
2023-08-18 23:28:46 +0545

    Update rdp_script.py

--- .github/scripts/rdp_script.py
+++ .github/scripts/rdp_script.py
@@ -23,11 +23,21 @@ def main():
     # Type the IP address of the RDP server
     pyautogui.write('YOUR_RDP_SERVER_IP')
     pyautogui.press('enter')
-    time.sleep(2)
+    time.sleep(10)  # Wait for the RDP connection to establish
 
     # Perform other necessary interactions (username, password, etc.)
     # This part depends on your RDP setup
+    # ...
+
+    # Install RustDesk using PowerShell
+    pyautogui.hotkey('win', 'r')
+    time.sleep(2)
+    pyautogui.write('powershell')
+    pyautogui.press('enter')
+    time.sleep(2)
+    pyautogui.write('iex (iwr https://raw.githubusercontent.com/rustdesk/rustdesk/master/scripts/install.ps1 -UseBasicP  \owerShell)')
+    pyautogui.press('enter')
+    time.sleep(10)  # Wait for RustDesk installation to complete
 
 if __name__ == "__main__":
     main()
-
